05376032daafba46354e38f91750b7401c2b9048
Sergio Aguilar <aguiserg@gmail.com>
2013-05-15 18:42:50 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RophProjectTracking::Application.config.secret_token = '5dbb49f529a4ad10585e6b77077f6c81fa3b1aac38fcdc0cd74c3303fe9ea868299c464f00185f6542d6a1b3c9dc863b2db4f1bce8871e446bf6823ea0fb3fc7'
