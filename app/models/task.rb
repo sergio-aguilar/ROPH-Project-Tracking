36088a3cb77e390f5dace0f0207427fb9b0b1b25
Sergio Aguilar <aguiserg@gmail.com>
2013-05-15 18:57:04 -0500

    Add Task model

---
+++ app/models/task.rb
@@ -0,0 +1,8 @@
+class Task < ActiveRecord::Base
+  belongs_to :project
+  belongs_to :admin_user
+  attr_accessible :admin_user_id, :project_id, :title
+  
+  validates :title, :project_id, :admin_user_id, :presence => true
+  validates :is_done, :inclusion => { :in => [true, false] }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130515235148) do
+ActiveRecord::Schema.define(:version => 20130515235542) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -50,6 +50,14 @@ ActiveRecord::Schema.define(:version => 20130515235148) do
     t.string   "title"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "tasks", :force => true do |t|
+    t.integer  "project_id"
+    t.integer  "admin_user_id"
+    t.string   "title"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
   end
 
 end
