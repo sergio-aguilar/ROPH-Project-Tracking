5b019c26d4ac14d4e0167be239becc4b284cd737
Sergio Aguilar <aguiserg@gmail.com>
2013-05-15 20:28:30 -0500

    Fix Task Tweak

--- app/admin/tasks.rb
+++ app/admin/tasks.rb
@@ -1,10 +1,5 @@
 ActiveAdmin.register Task do
-<<<<<<< HEAD
-<<<<<<< HEAD
-  scope :all, default: :true
-=======
   scope :all, :default => true
->>>>>>> tweak-task
   scope :due_this_week do |tasks|
     tasks.where('due_date > ? and due_date < ?', Time.now, 1.week.from_now)
   end
@@ -12,17 +7,9 @@ ActiveAdmin.register Task do
     tasks.where('due_date < ?', Time.now)
   end
   scope :mine do |tasks|
-<<<<<<< HEAD
-    tasks.where(admin_user_id: current_admin_user.id)
-  end
-  
-=======
->>>>>>> tweak-task
-=======
     tasks.where(:admin_user_id => current_admin_user.id)
   end
   
->>>>>>> tweak-task
   show do
     panel "Task Details" do
       attributes_table_for task do
@@ -30,16 +17,10 @@ ActiveAdmin.register Task do
         row("Title") { task.title }
         row("Project") { link_to task.project.title, admin_project_path(task.project) }
         row("Assigned To") { link_to task.admin_user.email, admin_admin_user_path(task.admin_user) }
-<<<<<<< HEAD
-        row ("Due Date") { task.due_date ? l(task.due_date, format: :long) : '-' }
-      end
-    end
-=======
         row("Due Date") { task.due_date? ? l(task.due_date, :format => :long) : '-' } 
       end
     end
 
->>>>>>> tweak-task
     active_admin_comments
   end
 end
