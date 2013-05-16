646234f747a02a348cac0454260f802df4c11432
Sergio Aguilar <aguiserg@gmail.com>
2013-05-15 20:32:00 -0500

    Add sidebar to Task model show view

--- app/admin/tasks.rb
+++ app/admin/tasks.rb
@@ -23,4 +23,11 @@ ActiveAdmin.register Task do
 
     active_admin_comments
   end
+  
+  sidebar "Other Tasks For This User", :only => :show do
+    table_for current_admin_user.tasks.where(:project_id => task.project) do |t|
+      t.column("Status") { |task| status_tag (task.is_done ? "Done" : "Pending"), (task.is_done ? :ok : :error) }
+      t.column("Title") { |task| link_to task.title, admin_task_path(task) }
+    end
+  end
 end
